ad87ecd3831a462f68012199fae0a6909b6d33f1
John Du Hart <john@compwhizii.net>
2011-05-29 13:29:37 -0400

    JSON Prettyprinting

--- library/XenApi/ViewRenderer/Json.php
+++ library/XenApi/ViewRenderer/Json.php
@@ -41,7 +41,38 @@ class XenApi_ViewRenderer_Json extends XenApi_ViewRenderer_Abstract
 	public function renderData(array $data)
 	{
 		return $this->_format(
-			XenForo_ViewRenderer_Json::jsonEncodeForOutput($data)
+			$this->_encodeJson($data)
 		);
+	}
+
+	/**
+	 * Returns a JSON representation of a value
+	 *
+	 * Code borrowed from MediaWiki
+	 *
+	 * @param  $data
+	 * @return void
+	 */
+	protected function _encodeJson($data)
+	{
+		// Some versions of PHP have a broken json_encode, see PHP bug
+		// 46944. Test encoding an affected character (U+20000) to
+		// avoid this.
+		if ( !function_exists( 'json_encode' ) || $this->_isHtml || strtolower( json_encode( "\xf0\xa0\x80\x80" ) ) != '\ud840\udc00' ) {
+			$this->_loadJsonService();
+			$json = new Services_JSON();
+			return $json->encode( $data, $this->_isHtml );
+		} else {
+			return json_encode( $data );
+		}
+	}
+
+	private function _loadJsonService()
+	{
+		if (class_exists('Services_JSON')) return;
+
+		global $fileDir;
+
+		require($fileDir . '/library/XenApi/Services/JSON.php');
 	}
 }
